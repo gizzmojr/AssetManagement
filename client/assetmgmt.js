e64e3895ee57c3f13401d3936afb845252bb4919
Matthew Gizzi <gizzmojr@gmail.com>
2017-03-01 00:32:48 -0500

    Generate basic table from 'all' query

--- client/assetmgmt.js
+++ client/assetmgmt.js
@@ -1,11 +1,45 @@
 // Globals
 var url = "http://";
 
+var rootDom = "#assetMgmt";
+
 function initAssetMgmt() {
-    httpGet("/test", function(response) {
-        console.log(response);
-    }, function(error) {
-        alert(error.message);
+    initUI();
+}
+
+function addRows(resObj) {
+    var table = document.getElementById("table");
+
+    resObj.forEach(function(asset) {
+        var row = document.createElement("tr");
+        row.className = "table-row";
+
+        Object.keys(asset).forEach(function(key) {
+            var cell = document.createElement("td");
+            cell.className = "table-cell";
+            cell.innerHTML = asset[key];
+            row.appendChild(cell);
+        });
+        table.append(row);
+    });
+}
+
+function createHeader(resObj) {
+    var table = document.getElementById("table");
+    var tr = document.createElement("tr");
+    tr.className = "table-row";
+    tr.id = "table-header";
+
+    var keys = Object.keys(resObj);
+    keys.forEach(function(key) {
+        var title = document.createElement("td");
+        title.className = "table-row-title";
+        title.innerHTML = key;
+        tr.appendChild(title);
+
+        document.getElementById("table").appendChild(tr);
+
+        return tr;
     });
 }
 
@@ -40,4 +74,19 @@ function httpGet(method, successCallback, errorCallback) {
     xhr.setRequestHeader("Accept", "application/json");
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send();
+}
+
+function initUI() {
+    var table = document.createElement("table");
+    table.id = "table";
+
+    document.querySelector(rootDom).appendChild(table);
+
+// Following would get called from a NAV element
+    httpGet("/all", function(response) {
+        createHeader(response[0]);
+        addRows(response);
+    }, function(error) {
+        alert(error.message);
+    });
 }
