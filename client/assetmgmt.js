348b34205b638455f5d157df674ac55e8c88a1e3
Matthew Gizzi <gizzmojr@gmail.com>
2017-03-05 22:58:33 -0500

    Sort table from header click Very slow performance, even localhost

--- client/assetmgmt.js
+++ client/assetmgmt.js
@@ -49,6 +49,10 @@ function createHeader(resObj) {
         var title = document.createElement("th");
         title.innerHTML = key;
         title.class = "table-title";
+        title.onclick = function() {
+            sortTable(title);
+        }
+
         tr.appendChild(title);
 
         table.appendChild(tr);
@@ -136,4 +140,59 @@ function httpGet(method, successCallback, errorCallback) {
 
 function initUI() {
     createNav();
+}
+
+function sortTable(n) { // https://www.w3schools.com/howto/howto_js_sort_table.asp
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.querySelector(".table-fill");
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc"; 
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.getElementsByTagName("TR");
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[n.cellIndex];
+            y = rows[i + 1].getElementsByTagName("TD")[n.cellIndex];
+            /*check if the two rows should switch place,
+            based on the direction, asc or desc:*/
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch= true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch= true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            //Each time a switch is done, increase this count by 1:
+            switchcount ++; 
+        } else {
+            /*If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again.*/
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
 }
